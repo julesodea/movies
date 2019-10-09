21d0ad52486990ae1aef53671787a1d09aef32c4
julesodea <jules.odea@gmail.com>
2019-10-09 15:13:06 +1300

    Updates

---
+++ precache-manifest.02dd3034e476eca9c380dd412421d3b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1802c43e0f3dca06de1f4e3547effef3",
+    "url": "/movieDB/index.html"
+  },
+  {
+    "revision": "bc7ab1849d98609d2562",
+    "url": "/movieDB/static/css/main.ab437d5f.chunk.css"
+  },
+  {
+    "revision": "6c14cb6fdb249e061af7",
+    "url": "/movieDB/static/js/2.78fed29f.chunk.js"
+  },
+  {
+    "revision": "bc7ab1849d98609d2562",
+    "url": "/movieDB/static/js/main.3a4d586c.chunk.js"
+  },
+  {
+    "revision": "fd8904eb3cb5bdb91c4c",
+    "url": "/movieDB/static/js/runtime-main.289b5c16.js"
+  },
+  {
+    "revision": "4ea3be0684631e67eb052fecc06f11f6",
+    "url": "/movieDB/static/media/logo.4ea3be06.png"
+  }
+]);
